50a77905603a2f37cd7ba598ed294111dffae3bd
MawmartA7 <aaronstmart@gmail.com>
2024-06-21 21:24:46 -0300

    feat: Creation of the Task class with attribute validation and getters/setters
    - Implementation of the Task class with its attributes and validation
    - Creation of the tasks table from the Task class
    - Use of attribute validation with validation dependency
    - Implementation of getters and setters for attributes

---
+++ src/main/java/br/com/todo/todo/models/Task.java
@@ -0,0 +1,76 @@
+package br.com.todo.todo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String name;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String description;
+
+    @Column(nullable = false)
+    @NotNull
+    private int priority;
+
+    @Column(nullable = false)
+    @NotNull
+    private boolean done;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+}
