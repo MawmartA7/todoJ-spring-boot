3f80324621a332bbc7aac8c3391157c2c0125b21
MawmartA7 <aaronstmart@gmail.com>
2024-06-24 15:37:44 -0300

    feat: Add TaskRepository using JpaRepository
    - Created TaskRepository interface to manage Task entities
    - Extended JpaRepository for basic CRUD operations

---
+++ src/main/java/br/com/todo/todo/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package br.com.todo.todo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.todo.todo.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
