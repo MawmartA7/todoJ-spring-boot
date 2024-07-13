b5ea1d69fdc670f9b376a8e6097471d7f54be034
MawmartA7 <aaronstmart@gmail.com>
2024-07-13 10:41:19 -0300

    test(TaskServicesTest): Add unit tests for getAllPendingTasks
    - Added two unit tests for the getAllPendingTasks method to
      verify it returns a list of TaskDTOs with the done property set to false.
    - Ensured it returns an empty list of TaskDTOs if no tasks with done
      as false are found in the database.
    - Added one more task to the list used for unit testing.

--- src/test/java/br/com/todo/todo/services/TaskServicesTest.java
+++ src/test/java/br/com/todo/todo/services/TaskServicesTest.java
@@ -40,22 +40,26 @@ public class TaskServicesTest {
     private TaskDTO taskDTO;
     private TaskDTO taskDTO2;
     private TaskDTO taskDTO3;
+    private TaskDTO taskDTO4;
     private List<TaskDTO> tasksDTO;
     private Task task;
     private Task task2;
     private Task task3;
+    private Task task4;
     private List<Task> tasks;
 
     @BeforeEach
     public void setup() {
         taskDTO = new TaskDTO(1L, "Task name", "Task description", 2, false);
         taskDTO2 = new TaskDTO(2L, "Other task name", "Other task description", 1, true);
-        taskDTO3 = new TaskDTO(3L, "Other task name", "Other task description", 1, true);
-        tasksDTO = Arrays.asList(taskDTO, taskDTO2, taskDTO3);
+        taskDTO3 = new TaskDTO(3L, "Other task name", "Other task description", 3, true);
+        taskDTO4 = new TaskDTO(4L, "Other task name", "Other task description", 1, false);
+        tasksDTO = Arrays.asList(taskDTO, taskDTO2, taskDTO3, taskDTO4);
         task = new Task(1L, "Task name", "Task description", 2, false);
         task2 = new Task(2L, "Other task name", "Other task description", 1, true);
-        task3 = new Task(3L, "Other task name", "Other task description", 1, true);
-        tasks = Arrays.asList(task, task2, task3);
+        task3 = new Task(3L, "Other task name", "Other task description", 3, true);
+        task4 = new Task(4L, "Other task name", "Other task description", 1, false);
+        tasks = Arrays.asList(task, task2, task3, task4);
     }
 
     @Nested
@@ -67,8 +71,9 @@ public class TaskServicesTest {
             when(repository.findAll()).thenReturn(tasks);
             List<TaskDTO> taskDTOListReturned = taskServices.getAllTasks();
             assertEquals(tasksDTO.get(1), taskDTOListReturned.get(0));
-            assertEquals(tasksDTO.get(2), taskDTOListReturned.get(1));
+            assertEquals(tasksDTO.get(3), taskDTOListReturned.get(1));
             assertEquals(tasksDTO.get(0), taskDTOListReturned.get(2));
+            assertEquals(tasksDTO.get(2), taskDTOListReturned.get(3));
             assertEquals(tasksDTO.size(), taskDTOListReturned.size());
             verify(repository, times(1)).findAll();
         }
@@ -125,7 +130,7 @@ public class TaskServicesTest {
             List<TaskDTO> taskDTOListReturned = taskServices.getAllDoneTasks();
             assertEquals(tasksDTO.get(1), taskDTOListReturned.get(0));
             assertEquals(tasksDTO.get(2), taskDTOListReturned.get(1));
-            assertEquals(tasksDTO.size() - 1, taskDTOListReturned.size());
+            assertEquals(tasksDTO.size() - 2, taskDTOListReturned.size());
             verify(repository, times(1)).findByDoneTrue();
         }
 
@@ -136,6 +141,30 @@ public class TaskServicesTest {
             List<TaskDTO> taskListReturned = taskServices.getAllDoneTasks();
             assertEquals(0, taskListReturned.size());
             verify(repository, times(1)).findByDoneTrue();
+        }
+    }
+
+    @Nested
+    public class getAllPendingTasks {
+
+        @Test
+        @DisplayName("Should be return a task list with pending tasks")
+        void shouldReturnedADoneTasksList() {
+            when(repository.findByDoneFalse()).thenReturn(Arrays.asList(task, task4));
+            List<TaskDTO> taskDTOListReturned = taskServices.getAllPendingTasks();
+            assertEquals(tasksDTO.get(3), taskDTOListReturned.get(0));
+            assertEquals(tasksDTO.get(0), taskDTOListReturned.get(1));
+            assertEquals(tasksDTO.size() - 2, taskDTOListReturned.size());
+            verify(repository, times(1)).findByDoneFalse();
+        }
+
+        @Test
+        @DisplayName("Should be return a empty task list with pending tasks")
+        void shouldReturnedAnEmptyDoneTasksList() {
+            when(repository.findByDoneFalse()).thenReturn(Arrays.asList());
+            List<TaskDTO> taskListReturned = taskServices.getAllPendingTasks();
+            assertEquals(0, taskListReturned.size());
+            verify(repository, times(1)).findByDoneFalse();
         }
     }
 }
