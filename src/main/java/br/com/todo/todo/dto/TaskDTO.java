147b2000119a472168b517a5f166253d810cfa2f
MawmartA7 <aaronstmart@gmail.com>
2024-07-18 19:59:01 -0300

    refactor(TaskDTO ): Update BeanValidation error messages
    - Modified error messages for the “name” and “priority”
      attributes in TaskDTO.

--- src/main/java/br/com/todo/todo/dto/TaskDTO.java
+++ src/main/java/br/com/todo/todo/dto/TaskDTO.java
@@ -4,10 +4,10 @@ import br.com.todo.todo.models.Task;
 import jakarta.validation.constraints.*;
 
 public record TaskDTO(
-                Long id, @NotBlank(message = "Message must be filled with characters") String name,
-                @NotBlank(message = "Description must be filled with characters") String description,
-                @NotNull(message = "Priority cannot be empty") @Positive(message = "Priority cannot be negative or zero") Integer priority,
-                @NotNull(message = "Done cannot be null") Boolean done) {
+        Long id, @NotBlank(message = "Name must be filled with characters") String name,
+        @NotBlank(message = "Description must be filled with characters") String description,
+        @NotNull(message = "Priority cannot be null") @Positive(message = "Priority cannot be negative or zero") Integer priority,
+        @NotNull(message = "Done cannot be null") Boolean done) {
 
     public TaskDTO(Task data) {
         this(data.getId(), data.getName(), data.getDescription(), data.getPriority(), data.getDone());
